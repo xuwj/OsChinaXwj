53c532abe6470a426b03944f29566cb3b5570623
xuwj <751733455@qq.com>
2015-06-11 16:21:01 +0800

    新增首页用户（我的信息）界面，并引入库KJFrameForAndroid

--- app/src/main/java/student/oschinaxwj/fragment/MyInformationFragment.java
+++ app/src/main/java/student/oschinaxwj/fragment/MyInformationFragment.java
@@ -7,17 +7,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import butterknife.ButterKnife;
+import student.oschinaxwj.R;
 import student.oschinaxwj.base.BaseFragment;
 
 /**
+ * 用户（我的信息）界面
  * Created by user on 2015/6/9.
  */
 public class MyInformationFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(MyInformationFragment.class.getSimpleName());
-        textView.setGravity(Gravity.CENTER);
-        return textView;
+        View view = inflater.inflate(R.layout.fragment_my_information,
+                container, false);
+        ButterKnife.inject(this, view);
+        initView(view);
+        return view;
     }
 }
